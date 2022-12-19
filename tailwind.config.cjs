9fd254d3a8d0cc65462d9d1adeff62b81c336570
engageintellect <jc9361@gmail.com>
2022-12-18 16:12:21 -0800

    cleanine

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,47 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "gradient-x": "gradient-x 20s ease infinite",
+        'gradient-y':'gradient-y 20s ease infinite',
+        "gradient-xy": "gradient-xy 20s ease infinite",
+      },
+      keyframes: {
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
+          },
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
