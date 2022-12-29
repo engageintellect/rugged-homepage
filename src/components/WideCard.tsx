ac9ef5653072aba2a4c7c61053c030871ab01b54
engageintellect <jc9361@gmail.com>
2022-12-29 03:20:02 -0800

    cleaning up

--- src/components/WideCard.tsx
+++ src/components/WideCard.tsx
@@ -15,7 +15,7 @@ export default function WideCard({
     <>
       <a href={url}>
         <div className="flex h-full flex-col items-center justify-center overflow-hidden rounded-lg bg-neutral-800 p-4 text-white xl:h-48 xl:transition-transform">
-          <div className="flex flex-col items-center justify-center hover:scale-105 md:w-[66vw] xl:w-full">
+          <div className="flex flex-col items-center justify-center transition-transform hover:scale-105 md:w-[66vw] xl:w-full">
             <div className="pb-2 text-center text-4xl font-semibold">
               {title}
             </div>
