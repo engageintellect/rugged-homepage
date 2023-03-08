d99339adfbf141d5857ba379ca092f09f46653e1
engageintellect <jc9361@gmail.com>
2023-03-07 18:56:20 -0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Testing from "../components/Testing";
 
+import Script from "next/script";
+
 const roboto = Roboto({
   weight: "400",
   subsets: ["latin"],
@@ -19,17 +21,23 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     msg: "Financing now available for all products.",
   });
   return (
-    <main className={roboto.className}>
-      <Alert
-        title={trpcMessage.data?.title ?? "loading..."}
-        msg={trpcMessage.data?.msg}
-        url={trpcMessage.data?.url}
-      />
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-      {/* <Testing /> */}
-    </main>
+    <>
+      <Script id="zoom-info" strategy="afterInteractive">
+        {`<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><meta http-equiv="Refresh" content="0; URL=https://zi.my.salesforce.com/sfc/p/1U0000019dCp/a/Do0000004Hf4/TBw4oL0eD1dQExQ5YXVJqMD0IpJoPKyFocXHPuXkhnI"></head><body><div>Attachment not opening? Click this link: <a href="https://zi.my.salesforce.com/sfc/p/1U0000019dCp/a/Do0000004Hf4/TBw4oL0eD1dQExQ5YXVJqMD0IpJoPKyFocXHPuXkhnI">websights-implementation-guide.pdf</a></div></html>`}
+      </Script>
+
+      <main className={roboto.className}>
+        <Alert
+          title={trpcMessage.data?.title ?? "loading..."}
+          msg={trpcMessage.data?.msg}
+          url={trpcMessage.data?.url}
+        />
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+        {/* <Testing /> */}
+      </main>
+    </>
   );
 };
 
