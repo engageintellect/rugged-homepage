6481f40a250f009ca94323a583df04b24bae4cf7
engageintellect <jc9361@gmail.com>
2023-03-25 01:04:22 -0700

    abstracting api key on server

--- src/pages/api/submitForm.ts
+++ src/pages/api/submitForm.ts
@@ -1,13 +1,10 @@
-// send get request to the api with headers
-
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function getData(
+export default async function SubmitForm(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  // console.log('this is the request body');
-  // console.log(req.body);
+  console.log(req.headers);
 
   try {
     const response = await fetch(
@@ -16,7 +13,7 @@ export default async function getData(
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer pat-na1-73e49bfb-d6a2-42ae-8663-a5a1b21af776`,
+          Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`,
         },
 
         body: JSON.stringify(req.body),
@@ -24,7 +21,6 @@ export default async function getData(
     );
 
     var responseObj = await response.json();
-    // console.log(responseObj);
 
     if (response.status >= 400) {
       return res
